551d52ad8d2f2e7e8530e9887a7d9d8ee1579a6d
AlexLordMetal <alexedbogdanov@gmail.com>
2018-11-01 17:15:26 +0300

    Added Homework2_4

---
+++ Homework2_4/Homework2_4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Homework2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задайте диапазон чисел.");
+            Console.Write("MIN: ");
+            int min = Int32.Parse(Console.ReadLine());
+            Console.Write("MAX: ");
+            int max = Int32.Parse(Console.ReadLine());
+            int[] numArray = new int[max - min];
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                numArray[i] = min + i;
+            }
+
+            Random random = new Random();
+            int j = 0;          // Additional value for random array element
+            int tempNum = 0;    // Additional value for changing array elements
+
+            for (int i = 0; i < numArray.Length; i++)     // Randomize array
+            {
+                j = random.Next(numArray.Length);
+                if (i != j)
+                {
+                    tempNum = numArray[i];
+                    numArray[i] = numArray[j];
+                    numArray[j] = tempNum;
+                }
+            }
+
+            Console.WriteLine("Массив с рандомом:");
+            foreach (int i in numArray)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < numArray.Length - 1; i++)     // Sort array
+            {
+                for (j = i + 1; j < numArray.Length; j++)
+                {
+                    if (numArray[j] < numArray[i])
+                    {
+                        tempNum = numArray[i];
+                        numArray[i] = numArray[j];
+                        numArray[j] = tempNum;
+                    }
+                }
+            }
+
+            Console.WriteLine("Отсортированный массив:");
+            foreach (int i in numArray)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
