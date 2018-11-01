6930470bd578c32b9ddb64e3f076755161a7d467
AlexLordMetal <alexedbogdanov@gmail.com>
2018-11-01 16:39:51 +0300

    Added Homework2_2_5

---
+++ Homework2_2_5/Homework2_2_5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
