dc0815031fc043f3978c259fbcc2d52089a4d0ab
AlexLordMetal <alexedbogdanov@gmail.com>
2018-11-01 16:57:09 +0300

    Added Homework2_1

---
+++ Homework2_1/Homework2_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = false;
+            bool b = false;
+            Console.WriteLine("  a\t  b\ta&&b\ta||b\t !a\t !b");
+            Console.WriteLine("_____________________________________________");
+            Console.WriteLine($"{a}\t{b}\t{a && b}\t{a || b}\t{!a}\t{!b}");
+            b = true;
+            Console.WriteLine($"{a}\t{b}\t{a && b}\t{a || b}\t{!a}\t{!b}");
+            a = true;
+            b = false;
+            Console.WriteLine($"{a}\t{b}\t{a && b}\t{a || b}\t{!a}\t{!b}");
+            b = true;
+            Console.WriteLine($"{a}\t{b}\t{a && b}\t{a || b}\t{!a}\t{!b}");
+            Console.ReadKey();
+        }
+    }
+}
