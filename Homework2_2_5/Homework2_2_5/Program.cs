60ed96d0f789f149c94291112e23ac2faaf03bed
AlexLordMetal <alexedbogdanov@gmail.com>
2018-11-01 16:47:32 +0300

    test

--- Homework2_2_5/Homework2_2_5/Program.cs
+++ Homework2_2_5/Homework2_2_5/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homework2_2_5
 {
@@ -10,6 +6,45 @@ namespace Homework2_2_5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Загадайте число от 0 до 100");
+            int startNum = 0;
+            int endNum = 100;
+            int midNum = 0;
+            int count = 1;
+            string isIt = "";
+            Console.Write("Загадали? ");
+            while (isIt != "=" && count < 8)
+            {
+                midNum = startNum + (endNum - startNum) / 2;
+                Console.WriteLine($"Попытка {count}: Ваше число {midNum} ? < = >");
+                isIt = Console.ReadLine();
+
+                if (isIt == "=")
+                {
+                    Console.WriteLine("Я угадал, я умная машина!");
+                }
+                else if (isIt == "<")
+                {
+                    endNum = midNum;
+                }
+                else if (isIt == ">")
+                {
+                    startNum = midNum + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Вы мухлюете, я пас!");
+                    isIt = "=";
+                }
+                count++;
+            }
+
+            if (isIt != "=" && count == 8)
+            {
+                Console.WriteLine("Вы мухлюете, я пас!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
